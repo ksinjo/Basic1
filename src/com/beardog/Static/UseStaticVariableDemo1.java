afffc756ce3d7f8888793a2534a9fbe9ccf52c7f
ksinjo <ksinjo@naver.com>
2017-07-14 21:41:57 +0900

    Static 변수를 사용하는 교제의 예제

---
+++ src/com/beardog/Static/UseStaticVariableDemo1.java
@@ -0,0 +1,38 @@
+package com.beardog.Static;
+
+/*
+ *  Static 변수를 사용 예제 
+ *   1. 불변의 상수와 같은 PI나 중력값을 사용 시 유용하다. 
+ *     
+ * 
+ */
+
+class Circle{
+	
+	static final double PI = 3.1415;
+	public double radius;
+	
+	public Circle(double rai){radius = rai;}
+	
+	public void showperimeter(){
+		double perim = radius *2 * PI;
+		System.out.println("원의 둘레는"+perim);
+	}
+	
+	public void showArea(){
+		double area = (radius * radius) * PI;
+		System.out.println("윈의 넓이는"+area);
+	}
+}
+	
+
+class UseStaticVariableDemo1 {
+
+public static void main(String[]args){
+	
+	 Circle cir1 = new Circle(5.721);
+	 cir1.showperimeter();
+	 cir1.showArea();
+	}
+
+}
